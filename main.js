00d0d6ac5da8d5631bb132fa5e16a6e87b047350
LunaVulperya <lunavulperya@gmail.com>
2025-05-30 20:28:12 -0300

    Fixed some code in JS to display the currency result correctly

--- main.js
+++ main.js
@@ -12,4 +12,14 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
         BRL: {USD:0.17, EUR:0.15},
         EUR: {USD:1.13, BRL:6.50}
     };
+
+    let valorConvertido;
+    if(daMoeda === paraMoeda){
+        valorConvertido = parseFloat(valor);
+    } else {
+        valorConvertido = parseFloat(valor * exchangeRates[daMoeda][paraMoeda]);
+    }
+
+    const conversao = document.getElementById('conversao');
+    conversao.textContent = `Resultado: ${valorConvertido.toFixed(2)} ${paraMoeda}`;
 });
